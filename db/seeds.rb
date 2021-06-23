befd3c797a8293d3b84be19604e2d892734132ad
Carlos Aguirre <craguirre952@gmail.com>
2021-06-22 20:36:27 -0400

    seed_dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-product = Product.new(name: "Fishing Pole", price: 22.95, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0mES1rcKvqbm2Hayu97w9oIr_4ALkvHeLmg&usqp=CAU", description: "every fisherman's every-fishing pole")
-
-
-product = Product.new(name: "Tank", price: 500000, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB0uT4MTL7L2aJhMCeKdHA7mr4GUXP5_nMUA&usqp=CAU", description: "a tank for every occassion!")
-
-
-product = Product.new(name: "baseball glove", price: 15.25, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOwDFJ8gvqwfFuayLmnitNHRW5QLKqmzMMAw&usqp=CAU", description: "the baseball glove you've always wanted")
-
+Product.create!([
+  {name: "Tank", price: "500000.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB0uT4MTL7L2aJhMCeKdHA7mr4GUXP5_nMUA&usqp=CAU", description: "a tank for every occassion!", inventory_count: nil},
+  {name: "rabbit", price: "50.0", image_url: "https://cf.ltkcdn.net/small-pets/images/orig/241114-1600x1030-flemish-giant-rabbit.jpg", description: "a big rabbit for you and your family", inventory_count: nil},
+  {name: "tent", price: "255.0", image_url: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_14/3462489/210406-camping-tent-bd-2x1.jpg", description: "go camping", inventory_count: nil},
+  {name: "train", price: "99999999.0", image_url: "https://images-na.ssl-images-amazon.com/images/I/71fFp-IVerL._AC_SL1500_.jpg", description: "go choo choo", inventory_count: nil},
+  {name: "car", price: "40000.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWXIltEVc7kNgO8dAYLD63WDk6A5OwQDU2QQ&usqp=CAU", description: "go vroom vroom", inventory_count: nil},
+  {name: "Fishing Pole", price: "22.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0mES1rcKvqbm2Hayu97w9oIr_4ALkvHeLmg&usqp=CAU", description: "the everyday fishing pole for the everyday person", inventory_count: nil},
+  {name: "rabbit", price: "50.0", image_url: "https://cf.ltkcdn.net/small-pets/images/orig/241114-1600x1030-flemish-giant-rabbit.jpg", description: "a big rabbit for you and your family", inventory_count: nil}
+])
