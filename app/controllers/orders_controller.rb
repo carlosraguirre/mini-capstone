8660a4a7dfdd4187776ba029f9396a074e639f3a
Carlos Aguirre <craguirre952@gmail.com>
2021-07-08 15:09:57 -0400

    update order serializer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
 
   def show
     order = Order.find_by(id: params[:id], user_id: current_user.id)
-    render json: order
+    render json: order, include: ['carted_products.product']
   end
 
 
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :subtotal, :tax, :total
+
+  has_many :carted_products
 end
